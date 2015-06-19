9af778d518a613c459e9752c5f79325ae9552bad
John Hurliman <jhurliman@jhurliman.org>
2015-06-18 23:06:20 -0700

    Progress on the queue demo app

--- SYNQueueDemo/SYNQueueDemo/ViewController.swift
+++ SYNQueueDemo/SYNQueueDemo/ViewController.swift
@@ -9,38 +9,68 @@
 import UIKit
 import SYNQueue
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    let queue = SYNQueue(queueName: "myQueue", maxConcurrency: 2, maxRetries: 5)
+    var tasks = [SYNQueueTask]()
+    var nextTaskID = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let queueName = "myQueue"
-        let queue = SYNQueue(queueName: queueName, maxConcurrency: 2, maxRetries: 10)
-        
-        queue.addTaskHandler("cellTask") { (task) in
-            // Do the task
-            
-            // Complete the task
-            task.completed(NSError(domain: "Queue", code: -1, userInfo: nil))
-        }
-        
-        let task = SYNQueueTask(queue: queue, taskID: "a", taskType: "cellTask",
-            dependencyStrs: [], data: [:])
-        
-        queue.addOperation(task)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        queue.addTaskHandler("cellTask", taskHandler: taskHandler)
     }
     
-    @IBAction func addTapped(sender: UIButton) {
+    override func viewDidLayoutSubviews() {
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.itemSize = CGSizeMake(collectionView.bounds.size.width, 50)
+        }
+    }
+    
+    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
+        collectionView.performBatchUpdates(nil, completion: nil)
+    }
+    
+    func taskHandler(task: SYNQueueTask) {
+        // Do the task
         
+        // Complete the task
+        task.completed(NSError(domain: "Queue", code: -1, userInfo: nil))
+    }
+    
+    // MARK: - UICollectionView Delegates
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let task = tasks[indexPath.item]
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("TaskCell", forIndexPath: indexPath) as! TaskCell
+
+        cell.taskID = tasks[indexPath.item].name
+        cell.backgroundColor = task.executing ? UIColor.blueColor() : UIColor.grayColor()
+        
+        return cell
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func addTapped(sender: UIButton) {
+        let task = SYNQueueTask(queue: queue, taskID: String(nextTaskID++), taskType: "cellTask",
+            dependencyStrs: [], data: [:])
+        tasks.append(task)
+        collectionView.reloadData()
+        queue.addOperation(task)
     }
     
     @IBAction func removeTapped(sender: UIButton) {
-        
+        // Find the first task in the list
+        if let task = tasks.first {
+            task.cancel()
+            tasks.removeAtIndex(0)
+            collectionView.reloadData()
+        }
     }
 }
