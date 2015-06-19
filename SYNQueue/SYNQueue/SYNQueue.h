e4a7a82dd1abb0c5ef12f61fdc59a3bfa54c6e33
John Hurliman <jhurliman@jhurliman.org>
2015-06-19 16:56:43 -0700

    Removed UIKit dependency from SYNQueue

--- SYNQueue/SYNQueue/SYNQueue.h
+++ SYNQueue/SYNQueue/SYNQueue.h
@@ -6,14 +6,10 @@
 //  Copyright (c) 2015 Syntertainment. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SYNQueue.
 FOUNDATION_EXPORT double SYNQueueVersionNumber;
 
 //! Project version string for SYNQueue.
 FOUNDATION_EXPORT const unsigned char SYNQueueVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <SYNQueue/PublicHeader.h>
-
-
