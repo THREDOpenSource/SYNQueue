41ff4826f3864526c665eb1504194c6b1fafa586
John Hurliman <jhurliman@jhurliman.org>
2015-06-21 01:34:46 -0700

    Added ConsoleLogger example

---
+++ SYNQueueDemo/SYNQueueDemo/ConsoleLogger.swift
@@ -0,0 +1,26 @@
+//
+//  ConsoleLogger.swift
+//  SYNQueueDemo
+//
+//  Created by John Hurliman on 6/20/15.
+//  Copyright (c) 2015 Syntertainment. All rights reserved.
+//
+
+import Foundation
+import SYNQueue
+
+func log(level: LogLevel, msg: String) {
+    return ConsoleLogger.log(level, msg)
+}
+
+class ConsoleLogger : SYNQueueLogProvider {
+    // MARK: - SYNQueueLogProvider Delegates
+    
+    func log(level: LogLevel, _ msg: String) {
+        return ConsoleLogger.log(level, msg)
+    }
+    
+    class func log(level: LogLevel, _ msg: String) {
+        runOnMainThread { println("[\(level.toString().uppercaseString)] \(msg)") }
+    }
+}
